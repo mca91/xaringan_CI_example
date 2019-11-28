731055b4eb2d7da15be6d1dbcce27b2a0d19ae0f
mca91 <martin.emwikts@googlemail.com>
2019-11-28 15:02:11 +0100

    recursive file search

--- _build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-Rscript -e "rmarkdown::render('slides.Rmd', 'xaringan::moon_reader')"
--- scripts/_build.sh
+++ scripts/_build.sh
@@ -1,2 +1,7 @@
-#!/usr/bin/r
-rmarkdown::render('slides.Rmd', 'xaringan::moon_reader')
+#!/bin/sh
+Rscript -e "xrngn_fls<-list.files(recursive = T, pattern = '.Rmd')[1]
+for (i in 1:length(xrngn_fls)) {
+    callr::r(function(x) rmarkdown::render(x, 'xaringan::moon_reader'), 
+    args = list(xrngn_fls[i]))
+    rm(list = setdiff(ls(), 'xrngn_fls')) 
+}"
